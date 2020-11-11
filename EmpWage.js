4031fcdd08ea2cad06edd9cc4c31d6ec883f9d6a
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-11 12:31:31 +0530

    [Rituparna]Added: Employee Problem

---
+++ EmpWage.js
@@ -0,0 +1,47 @@
+// Check if employee is Present or Absent
+const IS_ABSENT = 0;
+function checkEmpPrestOrAbsent(empCheck, day) {
+    if (empCheck == IS_ABSENT) {
+        console.log("On  " + day + " Employee is ABSENT");
+    }
+    else {
+        console.log("On  " + day + " Employee is PRESENT");
+    }
+}
+
+// Calculate Daily Wage
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+const MAX_WORKING_DAYS = 20;
+
+function getEmpWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+function calculateDailyWage(empHrs){
+    return empHrs*WAGE_PER_HOUR;
+}
+// Calculate wages till condition met
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let arrayDailyWage = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    checkEmpPrestOrAbsent(empCheck, totalWorkingDays);
+    totalEmpHrs += getEmpWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days: " + totalWorkingDays + " Total Hours: " + totalEmpHrs + " Total Wage: " + empWage);
