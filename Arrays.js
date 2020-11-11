cc8161c51743010bf9b87cf285d0e27610dab244
Ritu1698 <biswasrituparna83@gmail.com>
2020-11-11 11:30:02 +0530

    [Rituparna]Added: Triplets Whose Sum is Zero

--- Arrays.js
+++ Arrays.js
@@ -26,17 +26,17 @@
 
     }
 
-    console.log("Second Minimum: " + secondMin+" Second Maximum: " + secondMax);
+    console.log("Second Minimum: " + secondMin + " Second Maximum: " + secondMax);
     //With Sort
     arr.sort();
-    console.log("Second Minimum: " + arr[1]+" Second Maximum: " + arr[arr.length -2]);
+    console.log("Second Minimum: " + arr[1] + " Second Maximum: " + arr[arr.length - 2]);
 }
 
 //Prime factorization Of Number
 {
     let number = Math.floor(Math.random() * 100) + 1;
     let arrOfPrimeFactors = new Array();
-    console.log("Number: "+number);
+    console.log("Number: " + number);
     while (number % 2 == 0) {
         arrOfPrimeFactors.push(2);
         number /= 2;
@@ -51,7 +51,32 @@
         arrOfPrimeFactors.push(number);
     }
     console.log("Dispaying Prime Factor Array Contents")
-    for(arrOfPrimeFactor of arrOfPrimeFactors){
+    for (arrOfPrimeFactor of arrOfPrimeFactors) {
         console.log(arrOfPrimeFactor);
     }
+}
+
+//Sum of three Integer adds to ZERO
+{
+    function findTriplets(arr, n) {
+        let found = true;
+        let countTriplets = 0;
+        for (let index1 = 0; index1 < n - 2; index1++) {
+            for (let index2 = index1 + 1; index2 < n - 1; index2++) {
+                for (let index3 = index2 + 1; index3 < n; index3++) {
+                    if (arr[index1] + arr[index2] + arr[index3] == 0) {
+                        countTriplets++;
+                        console.log("Triplet "+countTriplets+": "+arr[index1] + " " + arr[index2] + " " + arr[index3]);
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        if (found == false)
+           console.log("Triplets Don't Exist!!!")
+
+    }
+    let arrtriplets = [0, -1, 2, -3, 1]; 
+    findTriplets(arrtriplets, arrtriplets.length);
 }
